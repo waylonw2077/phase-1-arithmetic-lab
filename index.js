3b027a1c4fbdb81073fc1cf5fa8060954167fc1e
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-21 16:11:41 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER) +1
+
+let num3 = 4
+let num4 = 16
+let mod = num3%num4
+let max = Math.max(1, 5, 10, 15, 20)
